d3f0bf851b3688c1415ab775a770a32252a494d6
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-04-18 18:41:36 -0400

    http

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var request = require('request');
 
 app.set('port', (process.env.PORT || 8080));
 
@@ -9,7 +10,14 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-
+// app.get('/weather',function(req,res){
+//   console.log("here");
+//   city = req.query.city;
+//   request('http://api.openweathermap.org/data/2.5/weather?q='+city+"&APPID=d0b723cbc19d59057399ef8030b6ae01", function(err,response){
+//     console.log(response.body);
+//     res.json(response.body)
+//   })
+// })
 var server = app.listen(app.get('port'), function () {
   console.log('App running');
 });
